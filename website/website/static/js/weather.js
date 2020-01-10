b25226af10a74226e764f5c3ae336ab157525b30
jbell69 <51940579+jbell69@users.noreply.github.com>
2020-01-10 09:27:06 -0600

    Add files via upload

---
+++ website/website/static/js/weather.js
@@ -0,0 +1,14 @@
+// Creating map object
+var map = L.map("map", {
+    center: [39.8283, -98.5795],
+    zoom: 5
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(map);
+
